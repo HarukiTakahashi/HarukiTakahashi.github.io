964eb81f75564d7c387733aa0805b26e1c12ad75
Haruki Takahashi <ee97064@gmail.com>
2018-07-05 11:41:46 +0900

    devvvv

---
+++ dev/Gyro/gyro.js
@@ -0,0 +1,23 @@
+const elDebug = document.querySelector('#debug');
+
+window.addEventListener(
+  'deviceorientation',
+  deviceOrientationHandler);
+
+/** デバイスが傾いたときのイベントです。 */
+function deviceOrientationHandler(event) {
+  //ジャイロセンサー情報取得
+  // X軸
+  const beta = event.beta;
+  // Y軸
+  const gamma = event.gamma;
+  // Z軸
+  const alpha = event.alpha;
+
+  // ログを出力する
+  let html = ``;
+  html += 'X回転 : ' + beta + '<br />';
+  html += 'Y回転 : ' + gamma + '<br />';
+  html += 'Z回転 : ' + alpha;
+  elDebug.innerHTML = html;
+}
